287f5a5f63e48e36fa2de0b5198d84938f3626a0
PatoGonzalezz <pato12lc@gmail.com>
2023-05-07 20:27:24 -0400

    Se establece conexión a la API y se hace modal

--- js/API.js
+++ js/API.js
@@ -1,5 +1,14 @@
-fetch("https://my-json-server.typicode.com/PatoGonzalezz/patoJson/productos")
+const URL = "https://my-json-server.typicode.com/PatoGonzalezz/patoJson/productos"
+fetch(URL)
 .then(res=>res.json())
-.then (response=>{
-    console.log(response)
-})
+.then (data=>mostrarData(data))
+.catch(error => console.log(error))
+
+const mostrarData = data =>{
+    console.log(data)
+    let body =''
+    for(let i = 1&&2&&3&&4&&5&&6; i<data.length;i++){
+        body+=`<tr><td>Producto: ${data[i].nombre} <br><br>Descripcion: ${data[i].descripcion}</td></tr>`
+    }
+    document.getElementById('data').innerHTML = body   
+}
